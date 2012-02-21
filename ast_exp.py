738247aab522997c6b397b3a71d26dee27842349
Cary Miller <Miller.Cary@gmail.com>
2012-02-20 18:07:00 -0700

    AST experiment

--- ast_exp.py
+++ ast_exp.py
@@ -30,42 +30,51 @@ def targ(msg, n):
         x = 11
         x = 33/n
         print msg
+        return 99
     finally:
         evil = locals()
         globals().update(locals())
 
 
+
+
+
+def bb(n):
+    print 'boooooo'
+    x = 3*n
+    return [n]*3
+
 def globalize(f):
-    '''
-    '''
-    f = copy.deepcopy(f)
-    
-    # new statment(s) to go in finally block
-    stmt1 = "evil = locals()"
-    stmt2 = "globals().update(locals())"
-    stmt_list = [stmt1, stmt2]
-    end = [ast.parse(s).body[0] for s in stmt_list]
+#    f = copy.deepcopy(f)
+    return f
 
     # parse the input func
     src = inspect.getsource(f)
     f_tree = ast.parse(src)
 
-    # create ast node 
-    tf = ast.TryFinally(lineno=1, col_offset=1)
-    tf = ast.TryFinally()
-    # if lineno or col_offset are omitted => error later
-    # NOTE lineno and col_offset are dealt with by fix_missing_locations.
+    if 1:
+        # new statment(s) to go in finally block
+        stmt1 = "evil = locals()"
+        stmt2 = "globals().update(locals())"
+        stmt_list = [stmt1, stmt2]
+        end = [ast.parse(s).body[0] for s in stmt_list]
 
-    tf.body = f_tree.body[0].body # original func body
-    tf.finalbody =  end 
+        # create ast node 
+        tf = ast.TryFinally(lineno=1, col_offset=1)
+        tf = ast.TryFinally()
+        # if lineno or col_offset are omitted => error later
+        # NOTE lineno and col_offset are dealt with by fix_missing_locations.
 
-    fname = f.__name__  + '_glob'
-    f_tree.body[0].name = fname
-    f_tree.body[0].body = [tf]
+        tf.body = f_tree.body[0].body # original func body
+        tf.finalbody =  end 
+
+        fname = f.__name__  + '_glob'
+        f_tree.body[0].name = fname
+        f_tree.body[0].body = [tf]
+
     f_tree = ast.fix_missing_locations(f_tree)
     code_ob = compile(f_tree, '<string>', 'exec') # Module
-    #    code_ob = compile(f_tree, '<string>', 'eval') # Expr
-    eval(code_ob)
+    eval(code_ob)   # bb is back
     return locals()[fname]
 
 
@@ -76,13 +85,36 @@ def rm():
     for name in evil:
         exec('del %s' %name, globals())
 
+def globalize(f):
+    # NOT a decorator
+    fname = f.func_name
+    fglob = f.func_globals
+    lines = inspect.getsourcelines(f)
+    t = ' try:'
+    fin = [' finally:', '  evil=locals()', '  globals().update(locals())']
+    res = [lines[0], t] + lines[1:] + fin
+    new_src = '\n'.join(res)
+    exec(new_src, fglob)
+    # Can make it a decorator with the line below but the func is already
+    # replaced by the line above.
+#    return eval(fname, fglob)   # pointless return
+
+
+
+def parrot(f):
+    return inspect.getsource(f)
+
+
+
+
 
 def fn(c):
     a, b = 2, 3
     return a+b+c/0
     return a+b+c
 
-fn_glob = globalize(fn)
+globalize(fn)
+#p = parrot(fn)
 
 
 def paradox():
@@ -97,7 +129,7 @@ def paradox():
 print paradox()
 
 
-@memoize
+#@globalize
 def fact(n):
     assert n==int(n) and n>=0
     if n==0: return 1
