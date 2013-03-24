7dbb12853697a3820f7f6d4a901ca39947df7dbd
Cary Miller <Miller.Cary@gmail.com>
2013-03-24 14:48:53 -0600

    data

---
+++ py_data_tools/urls.py
@@ -0,0 +1,10 @@
+urls = [
+    "amazon.com", "apple.com", 'deere.com', "google.com", "ibm.com", 
+    'motorola.com', 'samsung.com', 'toyota.com', "yahoo.com", 'www.bash.org',
+    'www.d3js.org', 'www.erlang.org', 'www.jquery.org', 'www.lisp.org',
+    'www.octave.org', 'www.perl.org', 'www.python.org', 'www.r.org',
+    'www.racket.org', 'www.ruby.org', 'www.sas.org', 'www.scala.org',
+    'www.scheme.org', 'www.sql.org', 'www.stata.org', 'www.w3.org',
+]
+
+
