5ba3b8f18ffa514dbea9998a2df8e9583e15c429
Cary Miller <Miller.Cary@gmail.com>
2013-07-28 10:36:13 -0600

    Do what I am thinking

---
+++ util/universal.py
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+def do_what_i_am_thinking(*a, **k):
+    '''Do whatever the user is thinking right now.
+    '''
+    for thought in head:
+        do(thought)
+
+
+def abracadabra():
+    return voila()
+
+def universal(*a, **k):
+    '''Perform arbitrary processing on arbitrary input.
+    Return arbitrary results.
+    '''
+    return [f(**k) for f in a]
+    # TODO flesh it out
+
+   
+
+
